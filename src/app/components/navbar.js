b4d7faa54fdd268dc8d4be7c0c122b0a87b83f92
arata-exe <thatsanachotawan.09@gmail.com>
2024-09-07 00:10:40 +0700

    fgh

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -44,7 +44,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           9Arm.Shop
         </a>
         <button
